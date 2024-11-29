b3b294cefe9f9a9258a90da3bd0272983e24c3db
rizwxxn <190094293+rizwxxn@users.noreply.github.com>
2024-11-29 20:40:43 +0530

    Create lego.sh

---
+++ lego.sh
@@ -0,0 +1,5 @@
+DOMAIN_NAME="byebye.rizwxxn.online" \
+    EMAIL="rizwan125023@gmail.com" \
+    DNS_PROVIDER="cloudflare" \
+    CLOUDFLARE_DNS_API_TOKEN="1EbD8onii7SuNg5PgIKNUG0GksVn692Olxge7j6L" \
+    ./lego.sh
