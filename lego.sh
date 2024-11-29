b828f72da24480fe5d84cbbe252934ef1406b1db
rizwxxn <190094293+rizwxxn@users.noreply.github.com>
2024-11-29 21:05:46 +0530

    Delete lego.sh

--- lego.sh
+++
@@ -1,5 +0,0 @@
-DOMAIN_NAME="byebye.rizwxxn.online" \
-    EMAIL="rizwan125023@gmail.com" \
-    DNS_PROVIDER="cloudflare" \
-    CLOUDFLARE_DNS_API_TOKEN="1EbD8onii7SuNg5PgIKNUG0GksVn692Olxge7j6L" \
-    ./lego.sh
